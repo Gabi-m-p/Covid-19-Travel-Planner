b09c5b24a2609296335f14cfe801cb8039b0aa7c
Mabsky <grimdango@gmail.com>
2022-01-15 20:54:19 +1100

    2nd branch push

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@ headers.append("Content-Type", "application/x-www-form-urlencoded");
 fetch("https://test.api.amadeus.com/v1/security/oauth2/token", {
     method: "POST",
     headers,
-    body: "grant_type=client_credentials&client_id=xI8qVT6AlfsQRwHtjnnnAZFBQR5PPsdi&client_secret=0jpefIf4PfAv2uDq"
 }).then(function (response) {
     return response.json();
 }).then(function (data) {
@@ -16,7 +15,6 @@ fetch("https://test.api.amadeus.com/v1/security/oauth2/token", {
 
 fetch("https://test.api.amadeus.com/v1/duty-of-care/disease/covid19-area-report",{
     method:"GET",
-    headers: {"Authorization":"Bearer ZsLtEAnTOy418iUysiiu559M93GS", "Access-Control-Allow-Origin":"*"},
     mode:"cors",
 }).then(function (response) {
     return response.json();
@@ -37,30 +35,46 @@ fetch("https://www.covid19-api.com/country?name=China&format=json"
 
 */
 
+
+
+
 function country_data (event) {
-event.preventDefault();
-var country_name = "France"
-var new_string = "https://www.covid19-api.com/country?name=" + country_name + "&format=json"
-fetch(new_string, {
-	"method": "GET",
-	"headers": {
+    event.preventDefault();
+    
+    var country_name = document.querySelector(".country_input").value
+    var new_string = "https://www.covid19-api.com/country?name=" + country_name + "&format=json"
+    
+    fetch(new_string, {
+	    "method": "GET",
+	    "headers": {
 		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
 		"x-rapidapi-key": "7656eb3a5emsh21a5b41565b69d6p110da4jsn033e5b9f1a60"
 	}
-})
-.then(function (response) {
-    return response.json();
-})
-.then(function(data) {
-	console.log(data);
-})
-.catch(err => {
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data) {
+	    console.log(data);
+
+        //Display total cases
+        document.querySelector(".covid_cases_content").innerHTML = data[0].confirmed.toString(); 
+
+        //Display total recoveries 
+        document.querySelector(".recoveries_content").innerHTML = data[0].recovered.toString(); 
+
+        //Display total critical cases 
+        document.querySelector(".critical_content").innerHTML = data[0].critical.toString(); 
+
+        //Display total deaths 
+        document.querySelector(".deaths_content").innerHTML = data[0].deaths.toString(); 
+
+
+    })
+    .catch(err => {
 	console.error(err);
-});
+    });
 }
-
-
-
 
 
 document.querySelector("#country_form").addEventListener('submit', country_data);
