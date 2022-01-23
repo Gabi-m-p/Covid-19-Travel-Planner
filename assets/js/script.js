36d311926adc00c4e58a6ef4ebd7d04b3e4664fc
Suraj Shrestha <surajshrestha121@hotmail.com>
2022-01-24 00:49:03 +1100

    final update, fixed history button and other small cosmetic changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,11 @@ var searchQuery = $(".searchQuery")
 var covidData = $(".covidData")
 var topFiveDestination = []
 var storedList;
+
+//load search history
 getSearchHistory();
 
-//Event Listners
-//User entry search
+//when search button is clicked run API's
 searchBttn.click(function(){
     if(inputCityName.val() == ""){
         showError()
@@ -19,24 +20,14 @@ searchBttn.click(function(){
         topFiveDestination = []
         getLocationData();
         resize();
-        covidData.css("display","block")
         storeSearchHistory();
         getSearchHistory();
     }
 })
-//History Button search
-$(".historyBttn").each(function(){
-    $(this).click(function(){
-        searchCityName = $(this).text()
-        getLocationData();
-        resize();
-        covidData.css("display","block")
-    })
-})
 
-//API function
-//First API to pull top 5 destination
+//First API to pull top five destinations
 function getLocationData(){
+    topFiveDestination = []
     var getGeoURL = 'https://api.opentripmap.com/0.1/en/places/geoname?name='+searchCityName+'&apikey=5ae2e3f221c38a28845f05b664787bf73f18d1d79a0f9b9fd9899b42'
     fetch(getGeoURL)
     .then(function(response){
@@ -55,8 +46,9 @@ function getLocationData(){
             return response.json();
         })
         .then(function(xidData){
+            $(".headerData").empty();
+            $(".headerData").append('<h3 class="customBackground rounded p-1">Top Tourist Destinations</h3>')
             listOfDestination.empty();
-            listOfDestination.append('<h5>Top Tourist Destinations</h5>')
             for (i=0;i<5;i++){
                 var xidId = xidData.features[i].properties.xid
                 var placesURL = 'https://api.opentripmap.com/0.1/en/places/xid/'+xidId+'?apikey=5ae2e3f221c38a28845f05b664787bf73f18d1d79a0f9b9fd9899b42'
@@ -66,18 +58,20 @@ function getLocationData(){
                 })
                 .then(function(placesData){
                     topFiveDestination.push(placesData)
-                    console.log(placesData)
                     var name = placesData.name
                     var previewImage = placesData.preview.source
                     var wikiText = placesData.wikipedia_extracts.text
-                    listOfDestination.append('<button type="button" onclick="rendersTopFiveDestinations(event)" class="list-group-item list-group-item-action card" id = "'+placesData.xid+'">'+name+'</button><div class="p-1"></div>')
-                    listOfDestination.children().eq(1).addClass("active")
+                    listOfDestination.append('<button type="button" onclick="rendersTopFiveDestinations(event)" class="customColor list-group-item list-group-item-action card" id = "'+placesData.xid+'">'+name+'</button><div class="p-1"></div>')
+                    listOfDestination.children().eq(0).addClass("active")
+                    $("#imageHolder").attr("src",topFiveDestination[0].preview.source);
+                    $("#imageDetails").text(topFiveDestination[0].wikipedia_extracts.text);
                 })
             }
         })
     })
 }
 
+//onClick when top-five destination is clicked
 function rendersTopFiveDestinations(event){
     for(i=0;i<topFiveDestination.length;i++){
         if(event.target.id === topFiveDestination[i].xid){
@@ -92,7 +86,6 @@ function rendersTopFiveDestinations(event){
 //Second API to pull COVID data
 function country_data () {
     var new_string = 'https://www.covid19-api.com/country/code?code='+$('#countryCode').html()+'&format=json'
-    console.log(new_string)
     fetch(new_string, {
         "method": "GET",
         "headers": {
@@ -106,27 +99,37 @@ function country_data () {
     .then(function(c_data) {
         var totalCovidCases =new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(c_data[0].confirmed)
         covidData.empty();
-        covidData.append('<h2 class = "bg-info text-white">Summary of COVID Cases '+c_data[0].country+'</h2><h4 class="covid_cases">Total Covid Cases</h4><h5 class="covid_cases_content">'+c_data[0].confirmed.toLocaleString()+'</h5><h4 class="recoveries">Recoveries</h4><h5 class="recoveries_content">'+c_data[0].recovered.toLocaleString()+'</h5><h4 class="critical">Critical Cases</h4><h5 class="critical_content">'+c_data[0].critical.toLocaleString()+'</h5> <h4 class="deaths">Deaths</h4><h5 class="deaths_content">'+c_data[0].deaths.toLocaleString()+'</h5>')
+        covidData.append('<h3 class="customBackground rounded p-1">COVID Summary - '+c_data[0].country+'</h3><div class="d-flex justify-content-center"><div class="shadow custom-card border p-2"><h6>Total Cases</h6><h7>'+c_data[0].confirmed.toLocaleString()+'</h7></div><div class="p-1"></div><div class="shadow custom-card border p-2"><h6>Recoveries</h6><h7>'+c_data[0].recovered.toLocaleString()+'</h7></div><div class="p-1"></div><div class="shadow custom-card border p-2"><h6>Critical Cases</h6><h7>'+c_data[0].critical.toLocaleString()+'</h7></div><div class="p-1"></div><div class="shadow custom-card border p-2"><h6>Deaths</h6><h7>'+c_data[0].deaths.toLocaleString()+'</h7></div><div class="p-1"></div></div>')
+        // covidData.append('<h4 class="covid_cases">Total Covid Cases</h4><h5 class="covid_cases_content">'+c_data[0].confirmed.toLocaleString()+'</h5><h4 class="recoveries">Recoveries</h4><h5 class="recoveries_content">'+c_data[0].recovered.toLocaleString()+'</h5><h4 class="critical">Critical Cases</h4><h5 class="critical_content">'+c_data[0].critical.toLocaleString()+'</h5> <h4 class="deaths">Deaths</h4><h5 class="deaths_content">'+c_data[0].deaths.toLocaleString()+'</h5>')
     })
 }
 
 //Utilities function
+
 //get history from local storage and render to page
 function getSearchHistory(){
     $(".searchHistory").empty();
     storedList = JSON.parse(localStorage.getItem("history"))
         if (storedList !== null) {
             if(storedList.length>3){
-                var slicedList = storedList.slice(1).slice(-3)
+                var slicedList = storedList.slice(1).slice(-5)
                 for(i=0; i < slicedList.length; i++){
-                    $(".searchHistory").append('<button type="button" class="col-12 btn btn-info text-muted historyBttn">'+ slicedList[i] +'</button><div class="p-1"></div>' )
-                }
-            }else{
+                $(".searchHistory").append('<button type="button" onclick="searchHistory(event)" id ="'+slicedList[i]+'"class="col-12 btn btn-info text-muted historyBttn">'+ slicedList[i] +'</button><div class="p-1"></div>' )        
+            }
+        } else{
             for(i=0; i < storedList.length; i++){
-            $(".searchHistory").append('<button type="button" class="col-12 btn btn-info text-muted historyBttn">'+ storedList[i] +'</button><div class="p-1"></div>' )
+            $(".searchHistory").append('<button type="button" onclick="searchHistory(event)" id ="'+storedList[i]+'" class="col-12 btn btn-info text-muted historyBttn">'+ storedList[i] +'</button><div class="p-1"></div>' )
             }        
         }
     }
+}
+
+//on click of history list
+function searchHistory(event){
+    searchCityName = event.target.id
+    getLocationData();
+    resize();
+    covidData.css("display","block")
 }
 //save search history to local storage
 function storeSearchHistory() {
@@ -150,8 +153,9 @@ function showError(){
         }
     },1000)
 }
-
+//resize content
 function resize(){
     searchQuery.removeClass("col-sm-12 col-lg-12");
-    searchQuery.addClass("col-sm-4 col-lg-4")
+    searchQuery.addClass("col-sm-3 col-lg-3")
+    covidData.css("display","block")
 }
